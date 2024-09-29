9bbede94e56b950491570dbddf1dfff7cddc9aea
fasihnasir <154458171+fasih-nasir@users.noreply.github.com>
2024-09-30 02:51:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // REACT HOOK 
 import { useState } from 'react'
+
 // BOOTSTRAP 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -13,6 +14,7 @@ import Create from './firebase/create';
 import Dashboard from './firebase/dash';
 import NotFound from './firebase/NotFound';
 import ThemeProvider from './context/themecontext';
+import Chatlist from './components/chatlist';
 // FILES
 
 function App() {
@@ -23,10 +25,11 @@ function App() {
     <ThemeProvider>
     <BrowserRouter>
    <Routes>
-    <Route path='/' element={<Login/>}></Route>
+    <Route path='/login' element={<Login/>}></Route>
     <Route path='/Create' element={<Create/>}></Route>
-    <Route path='/Todo' element={<Dashboard/>}></Route>
+    <Route path='/' element={<Dashboard/>}></Route>
     <Route path='*' element={<NotFound/>}></Route>
+    <Route path="/userchats/:email" element={<Chatlist />} />
    </Routes>
     </BrowserRouter>
     </ThemeProvider>
